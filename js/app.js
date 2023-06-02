548c740f3ebe58917f0ce57ad8ed4ded33784f5d
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-06-02 19:40:23 -0400

    added mostrar secciones functionality

--- js/app.js
+++ js/app.js
@@ -38,4 +38,12 @@ function guardarCliente() {
     const modalFormulario = document.querySelector("#formulario");
     const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
     modalBootstrap.hide();
+
+    //Mostrar secciones
+    mostrarSecciones();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll(".d-none");
+    seccionesOcultas.forEach(seccion => seccion.classList.remove("d-none"));
 }
